bc7d00d22a915542cbfa754b955141e9bf9fab93
Bigaston <bigaston1@gmail.com>
2020-02-06 13:40:18 +0100

    Securisation

--- podimage.js
+++ podimage.js
@@ -19,45 +19,50 @@ app.get("/", (req, res) => {
 
 app.get("/get/*", (req, res) => {
     b64 = new Buffer.from(req.path.replace("/get/", "")).toString('base64')
-    res.redirect(`/v1/i/${b64}.png`)
+    res.redirect(`/v1/i/${b64}`)
 });
 
 app.get("/v1/i/:feed", (req, res) => {
     var feed_url = Buffer.from(req.params.feed, 'base64').toString('utf8');
 
     parser.parseURL(feed_url, (err, feed) => {
-        if(feed.items[0].itunes.image == undefined) {
-            img = feed.itunes.image ? feed.itunes.image : feed.image.url
-        } else {
-            img = feed.items[0].itunes.image
-        }
-
-        var renderObj = {
-            image_url: img,
-            ep_title: feed.items[0].title,
-            pod_title: feed.title
-        }
-
-        template = fs.readFileSync(path.join(__dirname, "/web/template.mustache"), "utf8")
-        string = mustache.render(template, renderObj);
-        (async () => {
-            const browser = await puppeteer.launch({
-              defaultViewport: {
-                width: 850,
-                height: 220
-              },
-              headless: true,
-              args: ['--no-sandbox']
-            });
-            const page = await browser.newPage();
-            await page.setContent(string);
-            page.screenshot({omitBackground: true}).then((img) => {
-                browser.close().then(() => {
-                    res.setHeader("content-type", "image/png");
-                    res.end(img)
+        if (feed) {
+            if(feed.items[0].itunes.image == undefined) {
+                img = feed.itunes.image ? feed.itunes.image : feed.image.url
+            } else {
+                img = feed.items[0].itunes.image
+            }
+    
+            var renderObj = {
+                image_url: img,
+                ep_title: feed.items[0].title,
+                pod_title: feed.title
+            }
+    
+            template = fs.readFileSync(path.join(__dirname, "/web/template.mustache"), "utf8")
+            string = mustache.render(template, renderObj);
+            (async () => {
+                const browser = await puppeteer.launch({
+                  defaultViewport: {
+                    width: 850,
+                    height: 220
+                  },
+                  headless: true,
+                  args: ['--no-sandbox']
                 });
-            })
-        })();
+                const page = await browser.newPage();
+                await page.setContent(string);
+                page.screenshot({omitBackground: true}).then((img) => {
+                    browser.close().then(() => {
+                        res.setHeader("content-type", "image/png");
+                        res.end(img)
+                    });
+                })
+            })();
+        } else {
+            res.status(500).send(feed_url + " n'est pas un flux RSS")
+        }
+        
     })
 })
 
@@ -65,7 +70,11 @@ app.get("/v1/r/:feed", (req, res) => {
     var feed_url = Buffer.from(req.params.feed, 'base64').toString('utf8');
 
     parser.parseURL(feed_url, (err, feed) => {
-        res.redirect(feed.items[0].link)
+        if (feed) {
+            res.redirect(feed.items[0].link)
+        } else {
+            res.status(500).send(feed_url + " n'est pas un flux RSS")
+        }
     })
 })
 
@@ -73,19 +82,23 @@ app.get("/api/feed/*", (req, res) => {
     var feed_url = req.path.replace("/api/feed/", "")
 
     parser.parseURL(feed_url, (err, feed) => {
-        b64 = new Buffer.from(req.path.replace("/get/", "")).toString('base64')
-        resObj = {
-            img: process.env.HOST + "/v1/i/" + b64 + ".png",
-            redirect: process.env.HOST + "/v1/r/" + b64,
-            title: feed.title
+
+        if (feed) {
+            b64 = new Buffer.from(req.path.replace("/get/", "")).toString('base64')
+            resObj = {
+                img: process.env.HOST + "/v1/i/" + b64,
+                redirect: process.env.HOST + "/v1/r/" + b64,
+                title: feed.title
+            }
+            
+            res.header("Access-Control-Allow-Origin", process.env.HOST);
+            res.header("Access-Control-Allow-Methods", "GET");
+            res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+            res.status(200).json(resObj);
+        } else {
+            res.status(500).send(feed_url + " n'est pas un flux RSS")
         }
 
-        console.log(resObj)
-    
-        res.header("Access-Control-Allow-Origin", process.env.HOST);
-        res.header("Access-Control-Allow-Methods", "GET");
-        res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
-        res.status(200).json(resObj);
     })
 })
 
